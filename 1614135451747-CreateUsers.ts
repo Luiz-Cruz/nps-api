5ea0452dbf99ef7b7e2d3cf94881ba9a7a468993
Luiz Cruz <luizcruzdev@gmail.com>
2021-02-24 00:04:05 -0300

    feat: adding users migrations

---
+++ 1614135451747-CreateUsers.ts
@@ -0,0 +1,11 @@
+import {MigrationInterface, QueryRunner} from "typeorm";
+
+export class CreateUsers1614135451747 implements MigrationInterface {
+
+    public async up(queryRunner: QueryRunner): Promise<void> {
+    }
+
+    public async down(queryRunner: QueryRunner): Promise<void> {
+    }
+
+}
