0a760139981dae1d7efea9bee874b43234aec943
Luiz Cruz <luizcruzdev@gmail.com>
2021-03-01 11:05:41 -0300

    refactor: adding sendmail service template

--- src/services/SendMailService.ts
+++ src/services/SendMailService.ts
@@ -1,46 +1,40 @@
 import nodemailer, { Transporter } from "nodemailer";
-import { resolve } from 'path';
-import handlebars from 'handlebars';
-import fs from 'fs';
+
+import handlebars from "handlebars";
+import fs from "fs";
 
 class SendMailService {
     private client: Transporter;
+
     constructor() {
-        nodemailer.createTestAccount().then(account => {
+        nodemailer.createTestAccount().then((account) => {
             const transporter = nodemailer.createTransport({
                 host: account.smtp.host,
                 port: account.smtp.port,
                 secure: account.smtp.secure,
                 auth: {
                     user: account.user,
-                    pass: account.pass
-                }
-            });
-            this.client = transporter;
+                    pass: account.pass,
+                },
+          });
+      
+          this.client = transporter;
         });
     }
-    async execute(to: string, subject: string, body: string) {
-        const npsPath = resolve(__dirname, "..", "views", "npsMail.hbs");
-        const templateFileContent = fs.readFileSync(npsPath).toString("utf-8");
-        
-        const mailTemplateParse = handlebars.compile(templateFileContent)
 
-        const html = mailTemplateParse({
-            name: to,
-            title: subject,
-            description: body
-        })
-
+    async execute(to: string, subject: string, variables: object, path: string) {
+        const templateFileContent = fs.readFileSync(path).toString("utf8"); 
+        const mailTemplateParse = handlebars.compile(templateFileContent);  
+        const html = mailTemplateParse(variables);  
         const message = await this.client.sendMail({
             to,
             subject,
-            html: body,
-            from: "NPS <noreplay@nps.com.br>"
-        })
-        console.log('Message sent: %s', message.messageId);
-        console.log('Preview URL: %s', nodemailer.getTestMessageUrl(message));
+            html,
+            from: "NPS <noreplay@nps.com.br>",
+        }); 
+        console.log("Message sent: %s", message.messageId);
+        console.log("Preview URL: %s", nodemailer.getTestMessageUrl(message));
     }
-}
-
+}   
 
 export default new SendMailService();
